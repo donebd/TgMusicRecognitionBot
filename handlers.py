12af30d2dc44172e1204709cefd297243f17eff6
donebd <dinil.bd@mail.ru>
2022-12-03 23:23:08 +0300

    Issue-2: Remove code duplicate

--- handlers.py
+++ handlers.py
@@ -18,28 +18,18 @@ def get_deafult_message():
 
 def handle_voice(update, context):
     print("Handle voice message")
-    chat_id = update.message.from_user['id']
-
-    voice_file = context.bot.get_file(update.message.voice.file_id)
-    voice_file_path = voice_file.download(os.path.join(TEMP_DIR, update.message.voice.file_id + ".mp3"))
-    music_json_data = recognize_music_from_file(voice_file_path)
-    if music_json_data['result'] is None:
-        update.message.reply_text("Music not recognized")
-        return
-
-    music = get_music_name(music_json_data)
-    image = get_image_by_url(
-        music_json_data['result']['deezer']['contributors'][0]['picture_medium'],
-        music_json_data['result']['artist'] + ".jpg"
-    )
-    context.bot.sendPhoto(chat_id=chat_id, photo=open(image, 'rb'), caption=music)
+    handle_audio_file(update, context, context.bot.get_file(update.message.voice))
 
 
 def handle_audio(update, context):
     print("Handle audio message")
+    handle_audio_file(update, context, context.bot.get_file(update.message.audio))
+
+
+def handle_audio_file(update, context, audio):
     chat_id = update.message.from_user['id']
-    audio_file = context.bot.get_file(update.message.audio.file_id)
-    audio_file_path = audio_file.download(os.path.join(TEMP_DIR, update.message.audio.file_id + ".mp3"))
+    audio_file = context.bot.get_file(audio.file_id)
+    audio_file_path = audio_file.download(os.path.join(TEMP_DIR, audio.file_id + ".mp3"))
     music_json_data = recognize_music_from_file(audio_file_path)
     if music_json_data['result'] is None:
         update.message.reply_text("Music not recognized")
