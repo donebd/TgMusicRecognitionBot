5fca672c7d95f475d5f908c515711dbfea1e5e0c
donebd <dinil.bd@mail.ru>
2022-12-11 04:07:30 +0300

    Issue-#20: Unit tests ci

--- src/test/test.py
+++ src/test/test.py
@@ -10,7 +10,7 @@ class TestStringMethods(unittest.TestCase):
     def test_getMusicName_whenGetValidJson_expectValidNameString(self):
         # arrange
         expected_music_name = "Imagine Dragons - Warriors\nAlbum: Smoke + Mirrors (Deluxe)"
-        with open(os.path.join("resources", "imagine_dragons.json"), 'r') as f:
+        with open(os.path.join(__file__, "..", "resources", "imagine_dragons.json"), 'r') as f:
             data = f.read()
         json_data = json.loads(data)
 
@@ -23,7 +23,7 @@ class TestStringMethods(unittest.TestCase):
     def test_getAppleMusicUrl_whenGetValidJson_expectAppleMusicUrl(self):
         # arrange
         expected_music_url = "https://music.apple.com/us/album/warriors/1440831203?app=music&at=1000l33QU&i=1440831624&mt=1"
-        with open(os.path.join("resources", "imagine_dragons.json"), 'r') as f:
+        with open(os.path.join(__file__, "..", "resources", "imagine_dragons.json"), 'r') as f:
             data = f.read()
         json_data = json.loads(data)
 
@@ -36,7 +36,7 @@ class TestStringMethods(unittest.TestCase):
     def test_getSpotifyMusicUrl_whenGetValidJson_expectSpotifyMusicUrl(self):
         # arrange
         expected_music_url = "https://open.spotify.com/track/1sWeSMifj6Z6kZyI6z3bRc"
-        with open(os.path.join("resources", "imagine_dragons.json"), 'r') as f:
+        with open(os.path.join(__file__, "..", "resources", "imagine_dragons.json"), 'r') as f:
             data = f.read()
         json_data = json.loads(data)
 
